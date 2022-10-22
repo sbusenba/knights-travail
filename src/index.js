1d5d9892f1182429e9790d0562b9c7cfff8387aa
Steven Busenbarrick <sbusenba@gmail.com>
2022-10-22 11:42:15 -0700

    randomized start and end

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const knightMoves = require("./knightMoves")
 const knightView = require("./knightView")
 let body = document.querySelector("body")
-let array = knightMoves([1,1],[2,6])
+let x1 = math.round(Math.random()*8)
+let y1 = math.round(Math.random()*8)
+let x2 = math.round(Math.random()*8)
+let y2 = math.round(Math.random()*8)
+let array = knightMoves([x1,y1],[x2,y2])
+
 body.appendChild(knightView(array))
 console.log (`found a path with ${array.length-1} moves`)
 array.forEach((move,index)=>{
